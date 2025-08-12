6cdcc7b3326c30d8e8da6bba277ffd907d4edf46
ZandhiForce <whehdgnl1998@gmail.com>
2025-08-12 16:03:10 +0900

    Solve problems 1001 and 25304, and update files

---
+++ src/p25304/Main.java
@@ -0,0 +1,23 @@
+package p25304;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int recur = sc.nextInt();
+        int totalPrice = 0;
+        for(int i =0; i<recur; i++){
+            int value = sc.nextInt();
+            int quantity = sc.nextInt();
+            totalPrice += value*quantity;
+        }
+        if(x == totalPrice){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
